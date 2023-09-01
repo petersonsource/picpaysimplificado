433d5171ecdcc85564366f3668084be3da615244
Peterson dos Santos <peterson.opensource@hotmail.com>
2023-08-31 22:05:24 -0300

    feat: config TransectionRespository

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/repositories/TransectionRespository.java
@@ -0,0 +1,9 @@
+package com.picpaysimplificado.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransectionRespository extends JpaRepository<TransectionRespository, Long> {
+
+
+
+}
